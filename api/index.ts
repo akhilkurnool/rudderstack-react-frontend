7ab89e50ff52b0fa3207d5b9454a22a92c0689be
Akhil Kurnool <akhil1996kc@gmail.com>
2023-03-28 14:39:57 +0530

    chore: added APIs

---
+++ api/index.ts
@@ -0,0 +1,57 @@
+
+const BASE_URL = 'http://localhost:3001'
+import { Source } from '../interfaces';
+
+export const request = (url: string, body?: BodyInit, method='GET') => {
+  return fetch(BASE_URL+url, {
+    method,
+    body,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then((d) => d.json());
+}
+
+export const URL = {
+  sources: '/sources',
+  templates: '/templates',
+  source: (id: number) => `/source/${id}`
+}
+
+export const getAllSources = () => {
+  return request(URL.sources)
+    .then((data) => data)
+    .catch((error) => {
+      console.error('GET sources failed', error)
+      throw error;
+    });
+}
+
+export const getAllTemplates = () => {
+  return request(URL.templates)
+    .then((data) => data)
+    .catch((error) => {
+      console.error('GET sources failed', error)
+      throw error;
+    });
+}
+
+export const createOrUpdateSource = (source: Source) => {
+  const method = source.id ? 'PUT' : 'POST';
+  return request(URL.sources, JSON.stringify(source), method)
+    .then((data) => data)
+    .catch((error) => {
+      console.error('GET sources failed', error)
+      throw error;
+    });
+}
+
+export const deleteSource = (sourceId: number) => {
+  if (!sourceId) return Promise.resolve();
+  return request(URL.source(sourceId), null, 'DELETE')
+  .then((data) => data)
+  .catch((error) => {
+    console.error('DELETE source failed', error)
+    throw error;
+  });
+}
